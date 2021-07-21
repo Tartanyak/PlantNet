fa446e24cbc91659f864327cd6ce773df047d72f
Tartanyak <mat.buxton@gmail.com>
2021-07-21 23:30:35 +0200

    Update readTemperature.py

--- sensors/readTemperature.py
+++ sensors/readTemperature.py
@@ -1,7 +1,7 @@
 import time
 from grove.factory import Factory
 # connect to alalog pin 2(slot A2)
-PIN = 2
+PIN = 0
 sensor = Factory.getTemper("NTC-ADC", PIN)
 print('Detecting temperature...')
 while True:
