2a23492eb9faaa1094e8cfdf639d630c32d4432c
Tartanyak <mat.buxton@gmail.com>
2021-07-22 16:49:49 +0200

    pin int and things

--- sensors/readTemperature.py
+++ sensors/readTemperature.py
@@ -2,6 +2,7 @@ from grove.factory import Factory
 
 def read_temperature(sensor):
     #return '{} Celsius'.format(sensor.temperature)
+    print('{}'.format(sensor.temperature))
     return '{}'.format(sensor.temperature)
 
 def get_sensor(pin):
