c3d89fd8629f0948e9adc11d2ea6798f401a875e
Tartanyak <mat.buxton@gmail.com>
2021-07-22 16:50:58 +0200

    Update plant_monitor.py

--- plant_monitor.py
+++ plant_monitor.py
@@ -9,7 +9,7 @@ def main():
     print("Getting hostname")
     host_name = get_hostname()
     print("Hostname:" + host_name)
-    print("Getting sensor from pin " + constants.air_temp_pin)
+    print("Getting sensor from pin {}".format(constants.air_temp_pin))
     temperature_sensor = get_sensor(constants.air_temp_pin)
     print("Got sensor")
     print("Getting MQTT client with " + constants.clientPublishName + " and brokerAddress " + constants.brokerAddress) 
