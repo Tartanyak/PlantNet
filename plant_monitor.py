f3402ec2c4ecf6777339878a79e4e7922f71498b
Tartanyak <mat.buxton@gmail.com>
2021-07-22 15:40:09 +0200

    fix import errors

--- plant_monitor.py
+++ plant_monitor.py
@@ -1,22 +1,22 @@
 import constants
-from comms import queueHandler
-from lib import packageJSON
-from sensors import readTemperature
+from comms.queueHandler import get_client, publish_message
+from lib.packageJSON import package_temperature, get_now_string, get_hostname
+from sensors.readTemperature import get_sensor, read_temperature
 import time
 
 def main():
-    host_name = packageJSON.get_hostname()
-    temperature_sensor = readTemperature.get_sensor()
-    client = queueHandler.get_client(constants.clientPublishName, constants.topic_air_temperature)
+    host_name = get_hostname()
+    temperature_sensor = get_sensor()
+    client = get_client(constants.clientPublishName, constants.topic_air_temperature)
 
     while True:
         try:
 
-            current_temperature = readTemperature.read_temperature(temperature_sensor, constants.air_temp_pin)
-            message_json = packageJSON.package_temperature(current_temperature, 
-                packageJSON.get_now_string(constants.now_string_format),
+            current_temperature = read_temperature(temperature_sensor, constants.air_temp_pin)
+            message_json = package_temperature(current_temperature, 
+                get_now_string(constants.now_string_format),
                 host_name)
-            queueHandler.publish_message(client, message_json, constants.topic_air_temperature)
+            publish_message(client, message_json, constants.topic_air_temperature)
             time.sleep(10)
             break
         except KeyboardInterrupt:
