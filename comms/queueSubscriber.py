e1cd9c5ede5a59946c86e337baf4477788b41e4f
Tartanyak <mat.buxton@gmail.com>
2021-07-22 19:16:32 +0200

    Update queueSubscriber.py

--- comms/queueSubscriber.py
+++ comms/queueSubscriber.py
@@ -1,17 +1,16 @@
 import paho.mqtt.client as mqtt
 import time
-import constants
 
 def on_message(client, userdata, message):
     print("received message: " ,str(message.payload.decode("utf-8")))
 
 
 
-clientSubscribe = mqtt.Client(constants.clientSubscribeName)
-clientSubscribe.connect(constants.brokerAddress) 
+clientSubscribe = mqtt.Client("PlantReader")
+clientSubscribe.connect("192.168.178.44") 
 
 clientSubscribe.loop_start()
-clientSubscribe.subscribe(constants.topic_air_temperature)
+clientSubscribe.subscribe("MOISTURE")
 
 clientSubscribe.on_message = on_message 
 
