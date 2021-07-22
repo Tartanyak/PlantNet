463189030d0b9a1c031385a80355fcf1555a8f98
Tartanyak <mat.buxton@gmail.com>
2021-07-22 21:49:10 +0200

    Update plant_monitor.py

--- plant_monitor.py
+++ plant_monitor.py
@@ -8,10 +8,12 @@ import time
 
 def main():
     host_name = lib.packageJSON.get_hostname()
+
     if(constants.read_temperature):
         temperature_sensor = sensors.readTemperature.get_sensor(constants.air_temp_pin)
     if(constants.read_moisture):
         moisture_sensor = sensors.readMoisture.get_sensor(constants.moisture_pin, constants.moisture_dry_voltage, constants.moisture_wet_voltage)
+
     client =  comms.queueHandler.get_client(constants.clientPublishName, constants.brokerAddress)
 
     while True:
@@ -22,9 +24,13 @@ def main():
                 lib.packageJSON.get_now_string(constants.now_string_format),
                 host_name)
             comms.queueHandler.publish_message(client, message_json, constants.topic_air_temperature)
+
         if(constants.read_moisture):
             current_moisture = sensors.readMoisture.read_moisture(moisture_sensor)
             current_moisture_voltage = sensors.readMoisture.read_voltage
+            print(current_moisture)
+            print(current_moisture_voltage)
+
             message_json = lib.packageJSON.package_value(constants.moisture_label,
                 current_moisture, 
                 lib.packageJSON.get_now_string(constants.now_string_format),
