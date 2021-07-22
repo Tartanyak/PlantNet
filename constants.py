2fad483f1e4b3a5ef6276c42b0933c5f26004f9f
Tartanyak <mat.buxton@gmail.com>
2021-07-22 17:56:21 +0200

    moisture sensor

--- constants.py
+++ constants.py
@@ -3,6 +3,7 @@ clientPublishName = "PlantData"
 clientSubscribeName = "PlantReader"
 topic_air_temperature = "AIR_TEMP"
 topic_moisture = "MOISTURE"
+read_sensor_interval =2
 air_temp_pin = 0 #int
 moisture_pin = 2 #int
 now_string_format = "%H:%M:%S:%f"
---
+++ sensors/readMoisture.py
@@ -0,0 +1,32 @@
+from grove.adc import ADC
+import time
+
+class GroveCapMoistureSensor:
+    def __init__(self, channel, dry_voltage, wet_voltage):
+            self.channel = channel
+            self.adc = ADC()
+            self.dry_voltage = dry_voltage
+            self.wet_voltage = wet_voltage
+
+    @property
+    def moisture_percent(self):
+        adjusted_voltage = abs(self.voltage - self.dry_voltage)
+        percent = (self.wet_voltage/100)*adjusted_voltage
+        return percent
+    
+    @property
+    def voltage(self):
+        value = self.adc.read_voltage(self.channel)
+        return value
+
+def get_sensor(pin, dry_voltage, wet_voltage):
+    sensor = GroveCapMoistureSensor(pin, dry_voltage, wet_voltage)
+    return sensor
+
+PIN = 0
+sensor = get_sensor(PIN, 2048, 1298)
+print('Detecting temperature...')
+while True:
+    print('{} mV'.format(sensor.voltage))
+    time.sleep(2)
+    
