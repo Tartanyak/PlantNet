18af17b412a690ac688ede6a3f1139cd0ca6d5ec
Tartanyak <mat.buxton@gmail.com>
2021-07-22 16:45:23 +0200

    Update readTemperature.py

--- sensors/readTemperature.py
+++ sensors/readTemperature.py
@@ -1,7 +1,7 @@
 from grove.factory import Factory
 
 def read_temperature(sensor):
-    return sensor.temperature
+    return '{} Celsius'.format(sensor.temperature)
 
 def get_sensor(pin):
     sensor = Factory.getTemper("NTC-ADC", pin)
