18f1889c3636049cbe6a212e720700a46862a2b1
Tartanyak <mat.buxton@gmail.com>
2021-07-24 16:11:36 +0200

    Update plant_collector.py

--- plant_collector.py
+++ plant_collector.py
@@ -1,15 +1,13 @@
 import constants
 import comms.queueHandler 
 import lib.packageJSON 
-#import sensors.readTemperature 
-#import sensors.readMoisture
 #import paho.mqtt.client as mqtt
 import time
 import datalayer.dbConnection
 
 def main():
     print("Starting collector")
-    #client =  comms.queueSubscriber.get_client(constants.clientPublishName, constants.brokerAddress)
+    client =  comms.queueSubscriber.get_client(constants.clientPublishName, constants.brokerAddress)
     print("Opening DB")
 
     db_file = datalayer.dbConnection.get_config_parameter(
@@ -21,11 +19,11 @@ def main():
     db_connection = datalayer.dbConnection.get_db_connection(db_connection, db_file)
     datalayer.dbConnection.testPrintSql(db_connection, "SELECT * from locations")
 
-#def on_message(client, userdata, message):
-    #print("Collector received message: " ,str(message.payload.decode("utf-8")))
+def on_message(client, userdata, message):
+    print("Collector received message: " ,str(message.payload.decode("utf-8")))
 
-#def on_connect(client, userdata, flags, rc):
-    #print("Collector connection returned result: " + mqtt.connack_string(rc))
+def on_connect(client, userdata, flags, rc):
+    print("Collector connection returned result: " + mqtt.connack_string(rc))
 
 if __name__ == "__main__":
     main()
