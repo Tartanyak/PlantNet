5828fb4e707e39c8ab7c92d820fe72b5d8a36d6f
Tartanyak <mat.buxton@gmail.com>
2021-07-26 22:55:12 +0200

    Logs

--- plant_collector.py
+++ plant_collector.py
@@ -7,22 +7,25 @@ import signal
 import sys
 
 def signal_handler(sig, frame):
-    
+    #Clean exit
     print("Stopping MQTT client")
     pc.stop()
     print("Test details")
-    datalayer.dbConnection.testPrintSql(db_connection, "select * from plant_logs")
+    datalayer.dbConnection.testPrintSql(db_connection, "select * from plant_logs where log_variable = 'light'")
     print("Closing DB connection")
     db_connection.close()
     sys.exit(0)
 
-def test_theory(client, userdata, message):
+def record_log(client, userdata, message):
     message_json = lib.helpers.message_to_string(message) 
-    
-    print(datalayer.plantLogRecorder.create_log_sql(db_connection, message_json))
+    insert_sql = datalayer.plantLogRecorder.create_log_sql(db_connection, message_json)
+    datalayer.dbConnection.runSql(db_connection, insert_sql)
+    db_connection.commit()
+
+#Starting here
 
 signal.signal(signal.SIGINT, signal_handler)
-
+signal.signal(signal.SIGTERM, signal_handler)
 db_file = datalayer.dbConnection.get_config_parameter(
             datalayer.dbConnection.get_config_json(constants.dbConfigFilename),
             constants.dbFilenameConfigParameter
@@ -31,5 +34,5 @@ db_connection = None
 db_connection = datalayer.dbConnection.get_db_connection(db_connection, db_file)
 
 pc = comms.queueSubscriber.QueueSubscriber(constants.clientSubscribeName)
-pc.on_message = test_theory
+pc.on_message = record_log
 pc.run(constants.brokerAddress, constants.topic_subscribe)
